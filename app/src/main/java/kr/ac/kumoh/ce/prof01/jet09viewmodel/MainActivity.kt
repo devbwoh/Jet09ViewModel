0f4637be3c063db90953464706c34910198a67d9
devbwoh <bwoh@outlook.kr>
2024-01-19 07:17:41 +0900

    🚧 rememberSaveable 회전

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet09viewmodel/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet09viewmodel/MainActivity.kt
@@ -1,13 +1,37 @@
 package kr.ac.kumoh.ce.prof01.jet09viewmodel
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.prof01.jet09viewmodel.ui.theme.Jet09ViewModelTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,12 +45,109 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    var count1 by rememberSaveable { mutableIntStateOf(0) }
+    var count2 by rememberSaveable { mutableIntStateOf(0) }
+
     Jet09ViewModelTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
+            if (LocalConfiguration.current.orientation
+                    == Configuration.ORIENTATION_PORTRAIT) {
+                Column {
+                    Box (Modifier.weight(1F)) {
+                        Counter(
+                            Modifier.background(Color(0xFFFE7A36)),
+                            count1
+                        ) {
+                            count1 = it
+                        }
+                    }
+                    Box (Modifier.weight(1F)) {
+                        Counter(
+                            Modifier.background(Color(0xFF280274)),
+                            count2
+                        ) {
+                            count2 = it
+                        }
+                    }
+                }
+            }
+            else {
+                Row {
+                    Box (Modifier.weight(1F)) {
+                        Counter(
+                            Modifier.background(Color(0xFFFE7A36)),
+                            count1
+                        ) {
+                            count1 = it
+                        }
+                    }
+                    Box (Modifier.weight(1F)) {
+                        Counter(
+                            Modifier.background(Color(0xFF280274)),
+                            count2
+                        ) {
+                            count2 = it
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun Counter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    onChangeCount: (Int) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            //.weight(1F)
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(Color(0XFFE9F6FF)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = count.toString(),
+            modifier = modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            color = Color.White,
+            fontSize = 100.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Row {
+            Button(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1F),
+                onClick = {
+                    onChangeCount(count + 1)
+                }
+            ) {
+                Text("증가", fontSize = 30.sp)
+            }
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Button(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1F),
+                onClick = {
+                    if (count > 0)
+                        onChangeCount(count - 1)
+                }
+            ) {
+                Text("감소", fontSize = 30.sp)
+            }
         }
     }
 }
